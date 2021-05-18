cb625ee0650d143e554f41ee016a085c1513c033
Fiona Spooner <spoonerfiona@gmail.com>
2021-05-18 15:52:28 +0100

    adding code to download metadata and unzip

--- un_sdg/download.py
+++ un_sdg/download.py
@@ -4,9 +4,11 @@ import json
 import requests
 import pandas as pd
 import os
+import zipfile
 from io import BytesIO
 
-from un_sdg import INFILE
+
+from un_sdg import INFILE, METAPATH
 
 base_url = "https://unstats.un.org/sdgapi"
 
@@ -28,6 +30,22 @@ area_codes = [int(area['geoAreaCode']) for area in areas]
 url = f"{base_url}/v1/sdg/Goal/DataCSV"
 res = requests.post(url, data={'goal': goal_codes, 'areaCodes': area_codes})
 assert res.ok
-df = pd.read_csv(BytesIO(res.content))
+df = pd.read_csv(BytesIO(res.content), low_memory = False)
 df.to_csv(os.path.join(INFILE), index=False)
 
+
+# Download and unzip metadata
+
+zip_url = 'https://unstats.un.org/sdgs/metadata/files/SDG-indicator-metadata.zip'
+r = requests.get(zip_url)  
+with open(os.path.join(METAPATH, 'sdg-metadata.zip'), 'wb') as f:
+    f.write(r.content)
+
+with zipfile.ZipFile(os.path.join(METAPATH, 'sdg-metadata.zip'), 'r') as zip_ref:
+    zip_ref.extractall(METAPATH)
+
+files_in_directory = os.listdir(METAPATH)
+filtered_files = [file for file in files_in_directory if not file.endswith(".pdf")]
+for file in filtered_files:
+	path_to_file = os.path.join(METAPATH, file)
+	os.remove(path_to_file)
